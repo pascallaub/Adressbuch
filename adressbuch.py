9cdb3ac1c04c2201a9f4898e85148fb21889627d
pascallaub <nutritionexpertition@gmail.com>
2024-09-28 11:53:22 +0200

    V. 1.5 implemented change contact function

--- adressbuch.py
+++ adressbuch.py
@@ -47,10 +47,46 @@ def eintrag():
     kontakte_speichern(kontakte)
     print("Eintrag erfolgreich!")
 
+def change():
+    suche = input("Gib den Namen oder die Telefonnummer zum Ändern ein: ")
+
+    kontakte = kontakte_laden()
+    gefundene_kontakte = [
+        kontakt for kontakt in kontakte
+        if suche.lower() in kontakt['name'].lower() or suche in kontakt['telefonnummer']
+    ]
+
+    if gefundene_kontakte:
+        for idx, kontakt in enumerate(gefundene_kontakte, start=1):
+            print(f"{idx}: Name: {kontakt['name']}, Telefonnummer: {kontakt['telefonnummer']}")
+        auswahl = int(input(f"Wähle einen Kontakt (1-{len(gefundene_kontakte)}): "))
+        ausgewählter_kontakt = gefundene_kontakte[auswahl - 1]
+
+        next = input("Willst du den Namen oder die Telefonnummer ändern? N/T: ").lower()
+        if next == 'n':
+            name_neu = input("Gib den neuen Namen ein: ")
+            for kontakt in kontakte:
+                if kontakt == ausgewählter_kontakt:
+                    kontakt['name'] = name_neu
+                    break
+            kontakte_speichern(kontakte)
+            print(f"Name geändert zu: {name_neu}")
+        elif next == 't':
+            nummer_neu = input("Gib die neue Telefonnummer ein: ")
+            for kontakt in kontakte:
+                if kontakt == kontakte:
+                    kontakt['telefonnummer'] = nummer_neu
+                    break
+            kontakte_speichern(kontakte)
+            print(f"Telefonnummer geändert zu: {nummer_neu}")
+        else:
+            print("Falsche Eingabe!")
+
+
 def adressbuch():
     while True:
-        print("Welche Aktion möchtest du durchführen?\n 1. Neuen Eintrag erstellen\n 2. Kontakte durchsuchen\n 3. Alle Kontakte anzeigen\n 4. Beenden")
-        auswahl = input("1, 2, 3 oder 4: ")
+        print("Welche Aktion möchtest du durchführen?\n 1. Neuen Eintrag erstellen\n 2. Kontakte durchsuchen\n 3. Alle Kontakte anzeigen\n 4. Eintrag ändern\n 5. Eintrag löschen\n 6. Einträge alphabetisch anzeigen\n 7. Beenden")
+        auswahl = input("1, 2, 3, 4, 5, 6 oder 7: ")
         try:
             if auswahl == '1':
                 eintrag()
@@ -62,6 +98,13 @@ def adressbuch():
                 showall()
                 continue
             elif auswahl == '4':
+                change()
+                continue
+            elif auswahl == '5':
+                pass
+            elif auswahl == '6':
+                pass
+            elif auswahl == '7':
                 break
         except ValueError:
             print("Falsche Eingabe!")
